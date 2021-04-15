4cea6d7e3068765c07fd233ff9e847097e9ee12c
diegolima <diego@diegolima.org>
2021-04-15 20:49:48 -0300

    Display IP for a specific instance

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "cvp_cluster_nodes" {  
-  value = module.cvp_cluster.cluster_node[*]#.network_interface.0.access_config.0.nat_ip
+  value = module.cvp_cluster.cluster_node[*]["https://www.googleapis.com/compute/v1/projects/cvp-tests/zones/us-central1-a/instances/my-cvp-cluster-g48h"].network_interface.0.access_config.0.nat_ip
 }
