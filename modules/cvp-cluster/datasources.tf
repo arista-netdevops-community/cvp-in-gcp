60beef0ec4164d56c29ccb393c607da188fe4ad5
diegolima <diego@diegolima.org>
2021-04-16 09:02:06 -0300

    Correctly reference instances created by the group manager

--- modules/cvp-cluster/datasources.tf
+++ modules/cvp-cluster/datasources.tf
@@ -6,8 +6,12 @@ data "google_compute_instance_group" "cvp_cluster" {
   self_link = google_compute_instance_group_manager.cvp_nodes.instance_group
 }
 
+locals {
+  cluster_node_list = flatten(data.google_compute_instance_group.cvp_cluster.instances)
+}
+
 data "google_compute_instance" "cluster_node" {
-  for_each = data.google_compute_instance_group.cvp_cluster.instances
+  count = length(flatten(data.google_compute_instance_group.cvp_cluster.instances))
   
-  self_link = each.key
+  self_link = local.cluster_node_list[count.index]
 }
--- modules/cvp-cluster/outputs.tf
+++ modules/cvp-cluster/outputs.tf
@@ -2,5 +2,5 @@ output "cluster_nodes" {
   value = data.google_compute_instance_group.cvp_cluster.instances
 }
 output "cluster_node" {
-  value = data.google_compute_instance.cluster_node[*]
+  value = data.google_compute_instance.cluster_node
 }
