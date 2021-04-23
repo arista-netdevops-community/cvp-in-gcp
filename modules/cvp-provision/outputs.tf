9ecb45447d753c71d3e8effa7174e2981ef30ab6
diegolima <diego@diegolima.org>
2021-04-23 09:29:22 -0300

    Add ssh user as an output

--- modules/cvp-provision/outputs.tf
+++ modules/cvp-provision/outputs.tf
@@ -1,3 +1,6 @@
 output "cluster_node" {
   value = data.google_compute_instance.cluster_node
 }
+output "cluster_ssh_user" {
+  value = var.vm_admin_user
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
 output "cvp_cluster_nodes_ips" {
-  value = module.cvp_provision_nodes.cluster_node[*].network_interface.0.access_config.0.nat_ip
+  value =  module.cvp_provision_nodes.cluster_node[*].network_interface.0.access_config.0.nat_ip
+}
+output "cvp_cluster_ssh_user" {
+  value =  module.cvp_provision_nodes.cluster_ssh_user
 }
