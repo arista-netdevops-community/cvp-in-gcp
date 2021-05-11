1a2f9d5f0de3dc0fcf03837c47c089f7bfe74462
diegolima <diego@diegolima.org>
2021-05-11 10:40:54 -0300

    Remove unecessary outputs

--- outputs.tf
+++ outputs.tf
@@ -11,6 +11,6 @@ output "cvp_cluster_ssh_user" {
 output "cvp_deviceadd_instructions" {
   value = "Provisioning complete. To add devices use the following TerminAttr configuration:\nexec /usr/bin/TerminAttr -ingestgrpcurl=${local.ingest_addr} -cvcompression=gzip -ingestauth=${module.cvp_provision_nodes.cvp_ingest_key} -smashexcludes=ale,flexCounter,hardware,kni,pulse,strata -ingestexclude=/Sysdb/cell/1/agent,/Sysdb/cell/2/agent -ingestvrf=default -taillogs"
 }
-output "cluster_node_data" {
-  value = module.cvp_provision_nodes.cluster_node_data[*].result
-}
+#output "cluster_node_data" {
+#  value = module.cvp_provision_nodes.cluster_node_data[*].result
+#}
