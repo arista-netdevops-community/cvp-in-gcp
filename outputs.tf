967ef4506424d26dce8b59bfcc5496b8afe7d695
diegolima <diego@diegolima.org>
2021-06-30 07:21:19 -0300

    Improve outputs

--- outputs.tf
+++ outputs.tf
@@ -2,14 +2,13 @@ locals {
   ingest_addr = length(module.cvp_provision_nodes.cluster_node[*].network.public.address) > 1 ? "${join(":9910,", module.cvp_provision_nodes.cluster_node[*].network.public.address)}:9910" : "${module.cvp_provision_nodes.cluster_node[0].network.public.address}:9910"
 }
 
-output "cvp_cluster_nodes_ips" {
-  value = module.cvp_provision_nodes.cluster_node[*].network.public.address
+output "cvp_instances_credentials" {
+  value       = zipmap(module.cvp_provision_nodes.cluster_node[*].network.public.address, module.cvp_provision_nodes.cluster_node[*].ssh.username)
+  description = "Public IP addresses and usernames of the cluster instances."
 }
-output "cvp_cluster_ssh_user" {
-  value = module.cvp_provision_nodes.cluster_node[*].ssh.username
-}
-output "cvp_deviceadd_instructions" {
-  value = "Provisioning complete. To add devices use the following TerminAttr configuration:\nexec /usr/bin/TerminAttr -ingestgrpcurl=${local.ingest_addr} -cvcompression=gzip -ingestauth=${module.cvp_provision_nodes.cvp_ingest_key} -smashexcludes=ale,flexCounter,hardware,kni,pulse,strata -ingestexclude=/Sysdb/cell/1/agent,/Sysdb/cell/2/agent -ingestvrf=default -taillogs"
+output "cvp_terminattr_instructions" {
+  value       = "Provisioning complete. To add EOS devices use the following TerminAttr configuration:\nexec /usr/bin/TerminAttr -ingestgrpcurl=${local.ingest_addr} -cvcompression=gzip -ingestauth=${module.cvp_provision_nodes.cvp_ingest_key} -smashexcludes=ale,flexCounter,hardware,kni,pulse,strata -ingestexclude=/Sysdb/cell/1/agent,/Sysdb/cell/2/agent -ingestvrf=default -taillogs"
+  description = "Instructions to add EOS devices to the CVP cluster."
 }
 #output "cluster_node_data" {
 #  value = module.cvp_provision_nodes.cluster_node_data[*].result
