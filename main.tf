081df0e5f30d491acb4b5ef008af3427e9fb653f
diegolima <diego@diegolima.org>
2021-07-05 10:22:23 -0300

    [DOC] Lock provision module version

--- main.tf
+++ main.tf
@@ -238,7 +238,7 @@ module "cvp_cluster" {
 }
 
 module "cvp_provision_nodes" {
-  source         = "git::https://gitlab.aristanetworks.com/tac-team/cvp-ansible-provisioning.git"
+  source         = "git::https://gitlab.aristanetworks.com/tac-team/cvp-ansible-provisioning.git?ref=v3.0.0"
   cloud_provider = "gcp"
 
   nodes                             = module.cvp_cluster.nodes
