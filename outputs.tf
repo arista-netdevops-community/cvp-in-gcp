4791fffdc7557cb7be6b9747b1e191036578e1e8
tamas <tjplug144@gmail.com>
2021-07-09 12:54:17 +0100

    update TA config to use the new cv flags

--- outputs.tf
+++ outputs.tf
@@ -9,7 +9,7 @@ output "cvp_cluster_ssh_user" {
   value =  module.cvp_provision_nodes.cluster_ssh_user
 }
 output "cvp_deviceadd_instructions" {
-  value = "Provisioning complete. To add devices use the following TerminAttr configuration:\nexec /usr/bin/TerminAttr -ingestgrpcurl=${local.ingest_addr} -cvcompression=gzip -ingestauth=${module.cvp_provision_nodes.cvp_ingest_key} -smashexcludes=ale,flexCounter,hardware,kni,pulse,strata -ingestexclude=/Sysdb/cell/1/agent,/Sysdb/cell/2/agent -ingestvrf=default -taillogs"
+  value = "Provisioning complete. To add devices use the following TerminAttr configuration:\nexec /usr/bin/TerminAttr -cvaddr=${local.ingest_addr} -cvcompression=gzip -cvauth=${module.cvp_provision_nodes.cvp_ingest_key} -smashexcludes=ale,flexCounter,hardware,kni,pulse,strata -ingestexclude=/Sysdb/cell/1/agent,/Sysdb/cell/2/agent -cvvrf=default -taillogs"
 }
 #output "cluster_node_data" {
 #  value = module.cvp_provision_nodes.cluster_node_data[*].result
